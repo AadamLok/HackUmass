9de672af340b4b6f6f773030ca45958d56b003d3
Aadam Lokhandwala <alokhandwala@umass.edu>
2021-11-06 05:44:48 -0400

    Tested fetch with backend

--- Mobile App/driveSave/App.js
+++ Mobile App/driveSave/App.js
@@ -1,15 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { print: "Waiting."}
+  }
+
+  getMessage() {
+    return fetch('http://10.0.2.2:5000/')
+      .then((response) => response.json())
+      .then((json) => {
+        return json.status;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  async componentDidMount() {
+    let val = await this.getMessage();
+    this.setState({ print: val});
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>{this.state.print}</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
+
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
