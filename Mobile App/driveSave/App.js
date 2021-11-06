4527d7a28e934c55ffc122d92eb48f6e656449b6
vivjamba <85517762+vivjamba@users.noreply.github.com>
2021-11-06 03:57:49 -0400

    Testing Image and Title

--- Mobile App/driveSave/App.js
+++ Mobile App/driveSave/App.js
@@ -1,19 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Image style={styles.banner}
+        blurRadius = {7}
+        source = {{
+          top: 10,
+          width: 400,
+          height: 200,
+          uri: "https://cdn.pixabay.com/photo/2013/11/28/10/36/road-220058_1280.jpg",
+        }}
+      />
+      <Text style={styles.title}>Welcome to DriveSave!</Text>
+      <Text>Drive smart, drive safe, save lives. lmao</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+    
+  },
+  banner: {
+    flex: 0.2,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
