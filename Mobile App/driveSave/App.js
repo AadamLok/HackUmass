9c2b819e75609bf83279e3714c94a51fbb9eeb85
vivjamba <85517762+vivjamba@users.noreply.github.com>
2021-11-06 13:08:57 -0400

    Added Basic Login Components

--- Mobile App/driveSave/App.js
+++ Mobile App/driveSave/App.js
@@ -1,7 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
+import { useState } from 'react';
 import { render } from 'react-dom';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, TextInput, Image, StyleSheet, Text, View, SafeAreaView } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 class App extends Component {
   constructor() {
@@ -25,18 +29,71 @@ class App extends Component {
     this.setState({ print: val});
   }
 
+  Username() {
+    const [text, setText] = useState('');
+    return (
+      <View style={{padding: 5}}>
+        <TextInput
+          style={{height: 40}}
+          placeholder="Email"
+          onChangeText={text => setText(text)}
+          defaultValue={text}
+        />
+        {/* <Text style={{padding: 10, fontSize: 42}}>
+          {text.split(' ').map((word) => word && '').join(' ')}
+        </Text> */}
+      </View>
+    );
+  }
+
+  Password() {
+    const [text, setText] = useState('');
+    return (
+      <View style={{padding: 0}}>
+        <TextInput
+          style={{height: 40}}
+          placeholder="Password"
+          onChangeText={text => setText(text)}
+          defaultValue={text}
+        />
+        {/* <Text style={{padding: 10, fontSize: 42}}>
+          {text.split(' ').map((word) => word && '').join(' ')}
+        </Text> */}
+      </View>
+    );
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>{this.state.print}</Text>
-        <StatusBar style="auto" />
-      </View>
+      // <View style={styles.container}>
+      //   <Text>{this.state.print}</Text>
+      //   <StatusBar style="auto" />
+      // </View>
+      <SafeAreaView style={styles.container}>
+      <Image style={styles.banner}
+        blurRadius = {7}
+        source = {{
+          width: 400,
+          height: 200,
+          uri: "https://cdn.pixabay.com/photo/2013/11/28/10/36/road-220058_1280.jpg",
+        }}
+      />
+      <Text style={styles.title}>Welcome to DriveSave!</Text>
+      <Text>Drive smart, drive safe, save lives. lmao</Text>
+      <StatusBar style="auto" />
+      <Username></Username>
+      <Password></Password>
+      <Button
+        onPress={() => }
+        title="Submit"
+        color="#e0fa91"
+        accessibilityLabel="Click this button to log in"
+      />
+    </SafeAreaView>
     );
   }
 }
 
-
-export default App;
 
 const styles = StyleSheet.create({
   container: {
